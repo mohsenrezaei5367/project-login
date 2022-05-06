faccea67a9588f9b9981fd02f7b4d32a04953d43
mohsenrezaei5367 <mohsenrezaei5367@gmail.com>
2022-05-06 12:53:30 +0430

    commit

--- js/login.js
+++ js/login.js
@@ -2,30 +2,90 @@ let wrapper = $("body");
 let load = 0;
 let int = setInterval(blurring, 20);
 const scale = (number, inMin, inMax, outMin, outMax) => {
-  return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
+    return ((number - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 };
-function blurring() {
-  load++;
-  if (load > 99) {
-    clearInterval(int);
-  }
 
-  wrapper.css({
-    filter: `blur(${scale(load, 0, 100, 30, 0)}px)`,
-  });
+function blurring() {
+    load++;
+    if (load > 99) {
+        clearInterval(int);
+    }
+
+    wrapper.css({
+        filter: `blur(${scale(load, 0, 100, 30, 0)}px)`,
+    });
 }
 
 $(function () {
-  $("input").attr("required", true);
+    $("input").attr("required", true);
 });
 
-let container = document.getElementById("container");
+$(function () {
 
-toggle = () => {
-  container.classList.toggle("sign-in");
-  container.classList.toggle("sign-up");
-};
+    let container = document.getElementById("container");
 
-setTimeout(() => {
-  container.classList.add("sign-in");
-}, 50);
+    toggle = () => {
+        container.classList.toggle("sign-in");
+        container.classList.toggle("sign-up");
+    };
+
+    setTimeout(() => {
+        container.classList.add("sign-in");
+    }, 50);
+})
+
+// validation forms
+$(function () {
+    let registerForm = $("#register-form");
+    if (registerForm.length) {
+        registerForm.validate({
+            rules: {
+                username: {
+                    minlength: 2,
+                },
+                email: {
+                    email: true
+                },
+
+                password: {
+                    required: true
+                },
+                phone: {
+                    minlength:11,
+                    maxlength:11,
+                },
+            },
+            messages: {
+                username: {
+                    required: "Please enter your username",
+                    minlength: "Please, at least {0} characters are necessary"
+                },
+                email: "please enter your email",
+                password: "Please enter your password",
+                phone: {
+                    required: "Please enter your phone",
+                    minlength:'phone number is less than 11 digit',
+                    maxlength:"phone number is more than 11 digit"
+
+                },
+            },
+
+
+            submitHandler: function (form) {
+                form.submit();
+            }
+
+        })
+    }
+
+
+
+
+
+
+
+    $("#acceptRegister").on('click', function (e) {
+        e.preventDefault
+        console.log(e)
+    })
+})
